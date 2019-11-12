0e2a8af585fe5b649710d9851137bd1bd509f58c
zhoujianbang <zhoujianbang@meicai.cn>
2019-11-12 19:51:22 +0800

    fix

--- deploy.sh
+++ deploy.sh
@@ -14,6 +14,6 @@ git add -A
 git commit -m 'deploy'
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
-git push -f https://github.com/zhoujianbang/vip.io.git master:gh-pages
+git push -f https://github.com/zhoujianbang/vp.git master:gh-pages
 
 cd -
